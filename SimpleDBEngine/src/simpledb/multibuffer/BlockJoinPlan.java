4cd4126234e697476bd40c624f82ca58040ea956
cheunggalen <73517133+cheunggalen@users.noreply.github.com>
2022-02-21 20:07:27 +0800

    Add files related to block nested loops join

---
+++ SimpleDBEngine/src/simpledb/multibuffer/BlockJoinPlan.java
@@ -0,0 +1,112 @@
+package simpledb.multibuffer;
+
+import simpledb.tx.Transaction;
+import simpledb.record.*;
+import simpledb.query.*;
+import simpledb.materialize.*;
+import simpledb.plan.Plan;
+
+public class BlockJoinPlan implements Plan {
+   private Transaction tx;
+   private Plan lhs, rhs;
+   private String commonfield;
+   private Schema schema = new Schema();
+
+   /**
+    * Creates a block nested loop plan for the specified queries.
+    * @param lhs the plan for the LHS query
+    * @param rhs the plan for the RHS query
+    * @param tx the calling transaction
+    */
+   public BlockJoinPlan(Transaction tx, Plan lhs, Plan rhs, String commonfield) {
+      this.tx = tx;
+      this.lhs = new MaterializePlan(tx, lhs);
+      this.rhs = rhs;
+      this.commonfield = commonfield;
+      schema.addAll(lhs.schema());
+      schema.addAll(rhs.schema());
+   }
+
+   /**
+    * A scan for this query is created and returned, as follows.
+    * First, the method materializes its LHS and RHS queries.
+    * It then determines the optimal chunk size,
+    * based on the size of the materialized RHS file and the
+    * number of available buffers.
+    * It creates a chunk plan for each chunk, saving them in a list.
+    * Finally, it creates a multiscan for this list of plans,
+    * and returns that scan.
+    * @see simpledb.plan.Plan#open()
+    */
+   public Scan open() {
+      Scan leftscan = lhs.open();
+      TempTable tt = copyRecordsFrom(rhs);
+      return new BlockJoinScan(tx, leftscan, tt.tableName(), tt.getLayout(), commonfield);
+   }
+
+   /**
+    * Returns an estimate of the number of block accesses
+    * required to execute the query. The formula is:
+    * <pre> B(product(p1,p2)) = B(p2) + B(p1)*C(p2) </pre>
+    * where C(p2) is the number of chunks of p2.
+    * The method uses the current number of available buffers
+    * to calculate C(p2), and so this value may differ
+    * when the query scan is opened.
+    * @see simpledb.plan.Plan#blocksAccessed()
+    */
+   public int blocksAccessed() {
+      // this guesses at the # of chunks (blocks)
+      int avail = tx.availableBuffs(); //number of buffers
+      int size = new MaterializePlan(tx, rhs).blocksAccessed(); //number of accesses (IOs)
+      int numchunks = size / avail; //number of blocks
+      return rhs.blocksAccessed() +
+            (lhs.blocksAccessed() * numchunks); //cost (IOs)
+   }
+
+   /**
+    * Estimates the number of output records in the product.
+    * The formula is:
+    * <pre> R(product(p1,p2)) = R(p1)*R(p2) </pre>
+    * @see simpledb.plan.Plan#recordsOutput()
+    */
+   public int recordsOutput() {
+      return lhs.recordsOutput() * rhs.recordsOutput();
+   }
+
+   /**
+    * Estimates the distinct number of field values in the product.
+    * Since the product does not increase or decrease field values,
+    * the estimate is the same as in the appropriate underlying query.
+    * @see simpledb.plan.Plan#distinctValues(java.lang.String)
+    */
+   public int distinctValues(String fldname) {
+      if (lhs.schema().hasField(fldname))
+         return lhs.distinctValues(fldname);
+      else
+         return rhs.distinctValues(fldname);
+   }
+
+   /**
+    * Returns the schema of the product,
+    * which is the union of the schemas of the underlying queries.
+    * @see simpledb.plan.Plan#schema()
+    */
+   public Schema schema() {
+      return schema;
+   }
+
+   private TempTable copyRecordsFrom(Plan p) {
+      Scan   src = p.open(); 
+      Schema sch = p.schema();
+      TempTable t = new TempTable(tx, sch);
+      UpdateScan dest = (UpdateScan) t.open();
+      while (src.next()) {
+         dest.insert();
+         for (String fldname : sch.fields())
+            dest.setVal(fldname, src.getVal(fldname));
+      }
+      src.close();
+      dest.close();
+      return t;
+   }
+}
---
+++ SimpleDBEngine/src/simpledb/multibuffer/NestedLoopJoinScan.java
@@ -0,0 +1,111 @@
+package simpledb.multibuffer;
+
+import simpledb.query.Constant;
+import simpledb.query.Scan;
+
+public class NestedLoopJoinScan implements Scan {
+   private Scan lhs, rhs;
+   private String commonfield;
+
+   /**
+    * Create a product scan having the two underlying scans.
+    * @param s1 the LHS scan
+    * @param s2 the RHS scan
+    */
+   public NestedLoopJoinScan(Scan lhs, Scan rhs, String commonfield) {
+      this.lhs = lhs;
+      this.rhs = rhs;
+      this.commonfield = commonfield;
+      beforeFirst();
+   }
+
+   /**
+    * Position the scan before its first record.
+    * In particular, the LHS scan is positioned at 
+    * its first record, and the RHS scan
+    * is positioned before its first record.
+    * @see simpledb.query.Scan#beforeFirst()
+    */
+   public void beforeFirst() {
+      lhs.beforeFirst();
+      lhs.next();
+      rhs.beforeFirst();
+   }
+
+   /**
+    * Move the scan to the next record.
+    * The method moves to the next RHS record, if possible.
+    * Otherwise, it moves to the next LHS record and the
+    * first RHS record.
+    * If there are no more LHS records, the method returns false.
+    * @see simpledb.query.Scan#next()
+    */
+   public boolean next() {
+      if (rhs.next()) {
+            if (rhs.getVal(commonfield).equals(lhs.getVal(commonfield))) {
+               return true;
+            }
+            return rhs.next();
+      } else {
+         rhs.beforeFirst();
+         return rhs.next() && lhs.next();
+      }
+   }
+
+   /** 
+    * Return the integer value of the specified field.
+    * The value is obtained from whichever scan
+    * contains the field.
+    * @see simpledb.query.Scan#getInt(java.lang.String)
+    */
+   public int getInt(String fldname) {
+      if (lhs.hasField(fldname))
+         return lhs.getInt(fldname);
+      else
+         return rhs.getInt(fldname);
+   }
+
+   /** 
+    * Returns the string value of the specified field.
+    * The value is obtained from whichever scan
+    * contains the field.
+    * @see simpledb.query.Scan#getString(java.lang.String)
+    */
+   public String getString(String fldname) {
+      if (lhs.hasField(fldname))
+         return lhs.getString(fldname);
+      else
+         return rhs.getString(fldname);
+   }
+
+   /** 
+    * Return the value of the specified field.
+    * The value is obtained from whichever scan
+    * contains the field.
+    * @see simpledb.query.Scan#getVal(java.lang.String)
+    */
+   public Constant getVal(String fldname) {
+      if (lhs.hasField(fldname))
+         return lhs.getVal(fldname);
+      else
+         return rhs.getVal(fldname);
+   }
+
+   /**
+    * Returns true if the specified field is in
+    * either of the underlying scans.
+    * @see simpledb.query.Scan#hasField(java.lang.String)
+    */
+   public boolean hasField(String fldname) {
+      return lhs.hasField(fldname) || rhs.hasField(fldname);
+   }
+
+   /**
+    * Close both underlying scans.
+    * @see simpledb.query.Scan#close()
+    */
+   public void close() {
+      lhs.close();
+      rhs.close();
+   }
+}
